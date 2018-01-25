3c2d18accc84cbf3eb81abaf029aeaa2af4dc698
h20dragon <h20dragon@outlook.com>
2018-01-25 11:16:22 -0500

    Cleanup.

--- lib/zapiclient/commands/server_info.rb
+++ lib/zapiclient/commands/server_info.rb
@@ -5,10 +5,7 @@ module Zapiclient::Commands
 
   class ServerInfo < Command
 
-
     BASE_URL = '/public/rest/api/1.0/serverinfo'
-    # uri = 'https://prod-api.zephyr4jiracloud.com/connect/public/rest/api/1.0/serverinfo'
-
 
     def initialize()
       super(BASE_URL)
@@ -16,9 +13,8 @@ module Zapiclient::Commands
       puts __FILE__ + (__LINE__).to_s + " [ServerInfo.init]"
     end
 
-
     def sendRequest()
-      puts __FILE__ + (__LINE__).to_s + " [ServerInfo.sendRequest]: "
+      puts __FILE__ + (__LINE__).to_s + " [ServerInfo.sendRequest]: " if @debug
 
       @jwt = jwt()
 
@@ -29,25 +25,15 @@ module Zapiclient::Commands
       }
 
       @response = RestClient.get 'https://prod-api.zephyr4jiracloud.com/connect/public/rest/api/1.0/serverinfo', headers
-
-      # We are expecting a JSON formatted string (response)
-
-      if @debug
-        # puts "RESPONSE => #{@response.class.to_s}"
-        # puts "==> " + @response.methods.sort.to_s
-      end
-
       parsed = JSON.parse(@response)
-
-
       return parsed
     end
 
     def execute()
-      puts __FILE__ + (__LINE__).to_s + " [SourceInfo.execute]"
+      puts __FILE__ + (__LINE__).to_s + " [SourceInfo.execute]" if @debug
       @response = sendRequest()
-
-      puts JSON.pretty_generate @response
+      puts JSON.pretty_generate @response if @debug
+      @response
     end
 
   end
