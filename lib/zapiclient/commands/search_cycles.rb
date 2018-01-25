23aca5f32f5d951aa94fae298270b7a13d8b5905
h20dragon <h20dragon@outlook.com>
2018-01-25 11:14:43 -0500

    Updates.

--- lib/zapiclient/commands/search_cycles.rb
+++ lib/zapiclient/commands/search_cycles.rb
@@ -1,21 +1,21 @@
 require 'json'
 require_relative './command'
+require_relative '../base/../base/cycles'
 
 module Zapiclient::Commands
 
   class SearchCycles < Command
 
-    #BASE_URL = '/public/rest/api/1.0/cycles/search?projectId=13301&versionId=-1'
-    BASE_URL = '/public/rest/api/1.0/cycles/search?projectId={PROJECT_ID}&versionId={VERSION_ID}'
-    # uri = 'https://prod-api.zephyr4jiracloud.com/connect/public/rest/api/1.0/serverinfo'
+    BASE_URL = '/public/rest/api/1.0/cycles/search?projectId={PROJECT_ID}&versionId={VERSION_ID}&expand=executionSummaries'
 
 
     def initialize(opts={:projectId => '-1', :versionId => '-1'})
      # super(BASE_URL)
+      @projectId = opts[:projectId]
       tmpStr = BASE_URL.sub('{PROJECT_ID}', opts[:projectId])
       tmpStr = tmpStr.sub('{VERSION_ID}', opts[:versionId])
 
-
+      @debug = false
       puts "[tmpStr]: #{tmpStr}" if @debug
 
       setUri(tmpStr)
@@ -23,6 +23,10 @@ module Zapiclient::Commands
       puts __FILE__ + (__LINE__).to_s + " [SearchCycles.init]" if @debug
     end
 
+    def dump()
+      puts __FILE__ + (__LINE__).to_s + " [SearchCycles.dump]"
+      puts JSON.pretty_generate @response
+    end
 
     def sendRequest()
       puts __FILE__ + (__LINE__).to_s + " [SearchCycles.sendRequest]: " if @debug
@@ -37,24 +41,21 @@ module Zapiclient::Commands
 
       @response = RestClient.get(getBaseUrl + @uri, headers)
 
-      # We are expecting a JSON formatted string (response)
-
-      if @debug
-        # puts "RESPONSE => #{@response.class.to_s}"
-        # puts "==> " + @response.methods.sort.to_s
-      end
-
       parsed = JSON.parse(@response)
-
 
       return parsed
     end
 
+    # Returns Cycles
     def execute()
-      puts __FILE__ + (__LINE__).to_s + " [SourceInfo.execute]"
+      puts __FILE__ + (__LINE__).to_s + " [SearchCycles.execute]" if @debug
       @response = sendRequest()
 
-      puts JSON.pretty_generate @response
+      puts JSON.pretty_generate @response if @debug
+
+      @cycles = Zapiclient::Cycles.new(@response, @projectId)
+
+      return @cycles
     end
 
   end
